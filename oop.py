6d7fd15812e59ce82957dd6662bab03b3e74d419
Casey Marshall <casey.s.marshall@gmail.com>
2009-03-03 17:37:12 -0800

    initial files

---
+++ oop.py
@@ -0,0 +1,9 @@
+r"""ooppy is a simple module for programming event loops in python; its
+design is based on the liboop API."""
+
+__version__ = '0.0.1'
+__author__ = 'Casey Marshall <casey.s.marshall@gmail.com>'
+
+class EventLoop(object):
+	def __init__(self):
+		pass
---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from distutils import setup
+
+setup(name='ooppy',
+	  version='0.0.1',
+	  py_modules = ['oop'])
